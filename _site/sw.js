844f2e781d79f84d86b8d40ca1da6cb01cbead30
Joshua Kurz <josh.kurz@weightwatchers.com>
2019-05-29 22:27:33 -0400

    fix(site): adding new cta content

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "locksmith-20190529222339";
+var CACHE_NAME = "locksmith-20190529222701";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201905292223",
+        "/css/pixyll.css?201905292227",
         "/"
       ]);
     })
